bc0b99316b6ad0ab324b852d860ed404bea2e543
gioerramun <gianfranco.erramun@gmail.com>
2024-04-09 20:20:53 -0300

    finalizado y testeado coleccionPiezas y visitante. 0 errores

--- src/coleccionPiezas.cpp
+++ src/coleccionPiezas.cpp
@@ -9,50 +9,19 @@ TColeccionPiezas crearColeccionPiezasVacia(){
     return NULL;
 }
 
-// void insertarPiezaAlPrincipio(TColeccionPiezas &coleccionPiezas, TPieza pieza){
-//     TColeccionPiezas tmp = new rep_coleccionpiezas;
-//     tmp->pieza = pieza;
-//     tmp->sig = coleccionPiezas;
-//     coleccionPiezas = tmp;
-// }
-
-// void insertarPieza(TColeccionPiezas &coleccionPiezas, TPieza pieza){
-//     if (coleccionPiezas == NULL) insertarPiezaAlPrincipio(coleccionPiezas, pieza);
-//     else {
-//         if (idTPieza(pieza) < idTPieza(coleccionPiezas->pieza)) insertarPiezaAlPrincipio(coleccionPiezas, pieza);
-//         else insertarPieza(coleccionPiezas, pieza);
-//     }
-// }
-
-void insertarPiezaColeccionPiezas(TColeccionPiezas &coleccionPiezas, TPieza pieza){
+void insertarPiezaAlPrincipio(TColeccionPiezas &coleccionPiezas, TPieza pieza){
     TColeccionPiezas tmp = new rep_coleccionpiezas;
     tmp->pieza = pieza;
-    tmp->sig = NULL;
+    tmp->sig = coleccionPiezas;
+    coleccionPiezas = tmp;
+}
 
-    if (coleccionPiezas == NULL) {
-        coleccionPiezas = tmp;
-    }
-    else if (idTPieza(pieza) < idTPieza(coleccionPiezas->pieza)) {
-        tmp->sig = coleccionPiezas;
-        coleccionPiezas = tmp;
-    }
+void insertarPiezaColeccionPiezas(TColeccionPiezas &coleccionPiezas, TPieza pieza){
+    if (coleccionPiezas == NULL) insertarPiezaAlPrincipio(coleccionPiezas, pieza);
     else {
-        TColeccionPiezas recorrer = coleccionPiezas;
-        while ((recorrer->sig != NULL) && (idTPieza(pieza) > idTPieza(recorrer->pieza)))
-        {
-            recorrer = recorrer->sig;
-        }
-        if (idTPieza(pieza) > idTPieza(recorrer->pieza))
-        {
-            recorrer->sig = tmp;
-        }
-        else
-        {
-            tmp->sig = recorrer->sig;
-            recorrer->sig = tmp;
-        }
+        if (idTPieza(pieza) < idTPieza(coleccionPiezas->pieza)) insertarPiezaAlPrincipio(coleccionPiezas, pieza);
+        else insertarPiezaColeccionPiezas(coleccionPiezas->sig, pieza);
     }
-    
 }
 
 void imprimirColeccionPiezas(TColeccionPiezas coleccionPiezas){
@@ -69,15 +38,34 @@ bool esVaciaColeccionPiezas(TColeccionPiezas piezas){
 }
 
 bool existePiezaColeccionPiezas(TColeccionPiezas coleccionPiezas, int idPieza){
-    return true;
+    TColeccionPiezas recorrer = coleccionPiezas;
+    bool existe = false;
+    while ((recorrer != NULL) && !existe) {
+        existe = idTPieza(recorrer->pieza) == idPieza;
+        recorrer = recorrer->sig;
+    }
+    return existe;
 }
 
 TPieza obtenerPiezaColeccionPiezas(TColeccionPiezas coleccionPiezas, int idPieza){
-    return NULL;
+    TColeccionPiezas recorrer = coleccionPiezas;
+
+    while ((recorrer != NULL) && (idPieza != idTPieza(recorrer->pieza))) {
+        recorrer = recorrer->sig;
+    }
+
+    return recorrer->pieza;
 }
 
 void removerPiezaColeccionPiezas(TColeccionPiezas &coleccionPiezas, int idPieza){
-   
+    if (idTPieza(coleccionPiezas->pieza) != idPieza) removerPiezaColeccionPiezas(coleccionPiezas->sig, idPieza);
+    else {
+        TColeccionPiezas tmp = coleccionPiezas;
+        coleccionPiezas = coleccionPiezas->sig;
+        liberarTPieza(tmp->pieza);
+        delete tmp;
+        tmp = NULL;
+    }
 }
 
 void liberarColeccionPiezas(TColeccionPiezas &coleccionPiezas){
--- src/visitante.cpp
+++ src/visitante.cpp
@@ -1,38 +1,72 @@
 #include "../include/visitante.h"
 
+// En esta sección se implementará el módulo visitante.cpp. 
+// Cada elemento del tipo visitante almacenará un
+// id, la edad, su nombre y apellido
+
+// 1. Implemente la representación de visitante rep_visitante y 
+// las funciones crearTVisitante, imprimirTVisitante y liberarTVisitante. 
+// Tenga en cuenta que el formato de impresión se especifica en visitante.h.
+// Ejecute el caso de prueba visitante1-crear-imprimir-liberar para verificar el 
+// funcionamiento de las operaciones. Foro de dudas.
+
+// 2. Implemente las funciones idTVisitante, nombreTVisitante, apellidoTVisitante 
+// y edadTVisitante. Ejecute el test visitante2-id-nombre-apellido-edad para 
+// verificar las funciones. Foro de dudas.
+
+// 3. Implemente la función copiarTVisitante. Ejecute el test visitante3-copiar 
+// para verificar la función. Ejecute el test visitante4-combinado Foro de dudas.
+
+
 struct rep_visitante {
-    
+    int id;
+    int edad;
+    char nombre[MAX_NOMBRE];
+    char apellido[MAX_APELLIDO];
 };
 
 TVisitante crearTVisitante(int id, const char nombre[MAX_NOMBRE], const char apellido[MAX_APELLIDO], int edad){
-    return NULL; 
+    TVisitante visitante = new rep_visitante;
+    visitante->id = id;
+    visitante->edad = edad;
+    strcpy(visitante->nombre, nombre);
+    strcpy(visitante->apellido, apellido);
+    return visitante;
 }
 
 void nombreTVisitante(TVisitante visitante, char nombre[MAX_NOMBRE]){
-
+    strcpy(nombre, visitante->nombre);
 }
 
 void apellidoTVisitante(TVisitante visitante, char apellido[MAX_APELLIDO]){
-
+    strcpy(apellido, visitante->apellido);
 }
 
 int idTVisitante(TVisitante visitante){
-    return 0;
+    return visitante->id;
 }
 
 int edadTVisitante(TVisitante visitante){
-    return 0;
+    return visitante->edad;
 }
 
 void liberarTVisitante(TVisitante &visitante){
-
+    delete visitante;
+    visitante = NULL;
 }
 
 TVisitante copiarTVisitante(TVisitante visitante){
-    return NULL;
+    TVisitante copiaVisitante = new rep_visitante;
+    copiaVisitante->id = visitante->id;
+    copiaVisitante->edad = visitante->edad;
+    strcpy(copiaVisitante->nombre, visitante->nombre);
+    strcpy(copiaVisitante->apellido, visitante->apellido);
+    return copiaVisitante;
 }
 
 void imprimirTVisitante(TVisitante visitante){
-
+    printf("Visitante %s %s\n", visitante->nombre, visitante->apellido);
+    printf("Id: %d\n", visitante->id);
+    printf("Edad: %d\n", visitante->edad);
 }
 
